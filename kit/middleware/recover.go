933a1eae83baba2d7ca744243350e392a97145c8
Igor Zibarev <zibarev.i@gmail.com>
2017-07-17 23:42:50 +0300

    Add stacktrace in recover middleware

--- kit/middleware/recover.go
+++ kit/middleware/recover.go
@@ -3,13 +3,14 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 func Recover(next http.Handler, errorLogger *log.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				errorLogger.Printf("panic: %+v", err)
+				errorLogger.Printf("panic: %v %s", err, debug.Stack())
 				w.WriteHeader(500)
 			}
 		}()
